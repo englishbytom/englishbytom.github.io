1f5b7fc5b954a113597ec4c29959f5e255940000
Tomas <tomas@studionumero.com>
2025-08-23 22:07:42 -0300

    Changed next config to mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
-// next.config.js
-const createNextIntlPlugin = require("next-intl/plugin");
+// next.config.mjs
+import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
-  // output: 'export', // keep commented if you don't want static export
+  // output: 'export',
   pageExtensions: ["js", "jsx", "ts", "tsx"],
   images: {
     unoptimized: true // needed if you use next/image with static export
   }
 };
 
-module.exports = withNextIntl(nextConfig);
+export default withNextIntl(nextConfig);
--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -22,6 +22,8 @@ export async function generateStaticParams() {
   return [{ locale: "en" }];
 }
 
+export const dynamic = "force-dynamic";
+
 export default async function LocaleLayout({
   children,
   params,
